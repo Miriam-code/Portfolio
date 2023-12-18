288bdfca80f82838c421f102ae7da53a6f257fdc
M'DWWM <miriamladjoui@gmail.com>
2023-12-18 18:03:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://miriam-code.github.io',
+  base: '/Portfolio',
+});
